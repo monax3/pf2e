cf3b67f1981e99eb18d0e070fcf00d6a1ab5081a
websterguy <54455090+websterguy@users.noreply.github.com>
2025-08-03 13:10:19 -0700

    Fix rule element forms not updating the data (#19802)

--- src/module/item/base/sheet/sheet.ts
+++ src/module/item/base/sheet/sheet.ts
@@ -631,7 +631,7 @@ class ItemSheetPF2e<TItem extends ItemPF2e> extends fav1.sheets.ItemSheet<TItem,
 
             const incomingData = expanded.system?.rules?.[idx];
             if (incomingData) {
-                ruleForm.updateObject(incomingData);
+                ruleForm.updateObject(incomingData as RuleElementSource);
                 itemRules[idx] = ruleForm.rule;
                 this.item.update({ "system.rules": itemRules });
             }
