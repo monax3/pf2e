a10ef280b40bafe6aa8d3c0cc7cd2add98471ad1
Mona <656889+monax3@users.noreply.github.com>
2025-08-09 01:10:24 +0200

    types: Define mixin interfaces separately allow declaration generation

--- src/module/actor/character/apps/formula-picker/app.ts
+++ src/module/actor/character/apps/formula-picker/app.ts
@@ -3,7 +3,6 @@ import type { CraftingAbility } from "@actor/character/crafting/ability.ts";
 import { CharacterPF2e } from "@actor/character/document.ts";
 import { ResourceData } from "@actor/creature/index.ts";
 import type { ApplicationConfiguration, ApplicationRenderOptions } from "@client/applications/_types.d.mts";
-import type ApplicationV2 from "@client/applications/api/application.d.mts";
 import type { ItemUUID } from "@common/documents/_module.d.mts";
 import { AbilityItemPF2e, FeatPF2e, PhysicalItemPF2e } from "@item";
 import { ItemType, TraitChatData } from "@item/base/data/index.ts";
@@ -21,9 +20,7 @@ interface FormulaPickerConfiguration extends ApplicationConfiguration {
 }
 
 /** Creates a formula picker dialog that resolves with the selected item */
-class FormulaPicker extends SvelteApplicationMixin<
-    AbstractConstructorOf<ApplicationV2> & { DEFAULT_OPTIONS: DeepPartial<FormulaPickerConfiguration> }
->(foundry.applications.api.ApplicationV2) {
+class FormulaPicker extends SvelteApplicationMixin(foundry.applications.api.ApplicationV2) {
     static override DEFAULT_OPTIONS = {
         id: "{id}-formula-picker",
         position: {
