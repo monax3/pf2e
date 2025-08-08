1aaa6d638fdb645c28d926ba3b975a2dceb04e9d
Mona <656889+monax3@users.noreply.github.com>
2025-08-09 01:10:24 +0200

    types: Add v13 CONFIG.time elements
    https://foundryvtt.com/api/variables/CONFIG.time.html

--- src/global.ts
+++ src/global.ts
@@ -306,10 +306,6 @@ declare global {
             ruleElement: boolean;
         };
         PF2E: typeof PF2ECONFIG;
-        time: {
-            roundTime: number;
-            turnTime: number;
-        };
     }
 
     const CONFIG: ConfigPF2e;
