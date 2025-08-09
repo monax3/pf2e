9af3688cb12f1b5680751940b02a1371c8915c42
Mona <656889+monax3@users.noreply.github.com>
2025-08-09 22:42:05 +0200

    Remove outdated ClientSettingsMap setting causing a type error

--- src/global.ts
+++ src/global.ts
@@ -6,7 +6,6 @@ import type { AutomaticBonusProgression as ABP } from "@actor/character/automati
 import type { ElementalBlast } from "@actor/character/elemental-blast.ts";
 import type { FeatGroupData } from "@actor/character/feats/index.ts";
 import type { CheckModifier, Modifier, ModifierType, StatisticModifier } from "@actor/modifiers.ts";
-import type { SettingConfig } from "@client/_types.d.mts";
 import type Hotbar from "@client/applications/ui/hotbar.d.mts";
 import type Config from "@client/config.d.mts";
 import type WallDocument from "@client/documents/wall.d.mts";
@@ -344,12 +343,6 @@ declare global {
             lte: (a: number, b: number) => boolean;
             ne: (a: number, b: number) => boolean;
             ternary: (condition: boolean | number, ifTrue: number, ifFalse: number) => number;
-        }
-    }
-
-    namespace foundry {
-        interface ClientSettingsMap {
-            get(key: "pf2e.worldClock"): SettingConfig & { default: WorldClockSettingData };
         }
     }
 
