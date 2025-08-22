cc251c78f59cdcfbeedccd0fdec03fcda04928a6
Mona <656889+monax3@users.noreply.github.com>
2025-08-22 17:44:00 +0200

    types: Turn types into interfaces to enable namespace merging

--- src/module/scene/token-document/data.ts
+++ src/module/scene/token-document/data.ts
@@ -2,14 +2,15 @@ import type { DocumentFlags } from "@common/data/_module.d.mts";
 import type { ModelPropsFromSchema } from "@common/data/fields.d.mts";
 import type { TokenSchema } from "@common/documents/token.d.mts";
 
-type TokenFlagsPF2e = DocumentFlags & {
+interface TokenFlagDataPF2e {
     pf2e: {
         [key: string]: unknown;
         linkToActorSize: boolean;
         autoscale: boolean;
     };
-};
+}
+type TokenFlagsPF2e = TokenFlagDataPF2e & DocumentFlags;
 
 type DetectionModeEntry = ModelPropsFromSchema<TokenSchema>["detectionModes"][number];
 
-export type { DetectionModeEntry, TokenFlagsPF2e };
+export type { DetectionModeEntry, TokenFlagDataPF2e, TokenFlagsPF2e };
